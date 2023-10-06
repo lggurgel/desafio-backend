00829cb4a8936ad33b9b36804b3952289288dac9
Leonardo <leocordova96@gmail.com>
2023-10-06 14:50:19 -0300

    updade: movies urls and views

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AddMovieWathcedList
+from .views import AddMovieWathcedList, WatchedMovieListView
 
 urlpatterns = [
-    path('add-movie/', AddMovieWathcedList.as_view(), name='add-movie')
+    path('add-movies/', AddMovieWathcedList.as_view(), name='add-movie'),
+    path('watched-movies/', WatchedMovieListView.as_view(), name='watched-movies')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from .models import Movie
 from .serializers import MovieSerializer
@@ -10,3 +10,11 @@ class AddMovieWathcedList(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class WatchedMovieListView(ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Movie.objects.filter(user=self.request.user)
