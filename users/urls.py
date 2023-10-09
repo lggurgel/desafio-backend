9754ac8a48607138c301dc71cd2e53f35b3d8fd1
Leonardo <leocordova96@gmail.com>
2023-10-09 09:03:41 -0300

    updade:urls

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('movies/', include("movies.urls"))
-
 ]
