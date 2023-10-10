bfff786c0d72941a6ba7f5d30d5e2a699acf5fe3
Leonardo <leocordova96@gmail.com>
2023-10-10 14:34:26 -0300

    refactor: enums.py

--- users/models.py
+++ users/models.py
@@ -1,29 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.enums import GENRE_CHOICES, SELECT
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     location = models.CharField(max_length=30, null=False)
-
-    SELECT = 'Select Genre'
-    ACTION = 'Action'
-    COMEDY = 'Comedy'
-    DRAMA = 'Drama'
-    ROMANCE = 'Romance'
-    SCIFI = 'Science Fiction'
-    HORROR = 'Horror'
-
-    GENRE_CHOICES = [
-        (SELECT, 'Select Genre'),
-        (ACTION, 'Action'),
-        (COMEDY, 'Comedy'),
-        (DRAMA, 'Drama'),
-        (ROMANCE, 'Romance'),
-        (SCIFI, 'Science Fiction'),
-        (HORROR, 'Horror'),
-    ]
-
     favorite_film_genre = models.CharField(
         max_length=100,
         choices=GENRE_CHOICES,
