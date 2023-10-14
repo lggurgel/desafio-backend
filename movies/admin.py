2fc64e5cd4ba4009d0e7f7179805486f407a2f61
Leonardo <leocordova96@gmail.com>
2023-10-14 11:05:59 -0300

    update: movies models and admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Movie
 
+
 class MovieAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Movie._meta.fields]
-    list_display_links = ['title']
-    
+    list_display_links = ['id']
+
+
 admin.site.register(Movie, MovieAdmin)
--- movies/models.py
+++ movies/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from users.models import CustomUser
 from .enums import GENRE_CHOICES, SELECT
 
+
 class Movie(models.Model):
 
-    title = models.CharField(max_length=100)
     year = models.IntegerField()
     director = models.CharField(max_length=100)
     genre = models.CharField(
@@ -12,5 +12,6 @@ class Movie(models.Model):
         choices=GENRE_CHOICES,
         default=SELECT
     )
-    personal_rating = models.PositiveIntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
+    personal_rating = models.PositiveIntegerField(
+        choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')])
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
