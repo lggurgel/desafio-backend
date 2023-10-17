154cba0589864861b67d86c68409441ca8229728
Leonardo <leocordova96@gmail.com>
2023-10-17 17:58:01 -0300

    refactor

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,11 @@
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from django.http import Http404
 from movies.models import Movie
 from movies.serializers import MovieSerializer
+from ranking.models import Ranking
 
 class AddMovieWathcedList(CreateAPIView):
     queryset = Movie.objects.all()
@@ -19,13 +22,13 @@ class UpdateMovieWatched(RetrieveUpdateAPIView):
 
     def get_object(self):
         movie_pk = self.kwargs.get('pk')
-        movie = Movie.objects.filter(pk=movie_pk, user=self.request.user).first()
+        movie = Movie.objects.filter(pk=movie_pk).first()
 
         if not movie:
             raise Http404
         return movie 
 
-class WatchedMovieListView(ListAPIView):
+class CreatedMovieListView(ListAPIView): #filmes que user adicionou
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAuthenticated]
@@ -36,7 +39,14 @@ class WatchedMovieListView(ListAPIView):
 class ListAllMovies(ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title', 'director']
+    filterset_fields = ['genre']
+    pagination_class = 2
+    
     permission_classes = [AllowAny]
  
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
+    
