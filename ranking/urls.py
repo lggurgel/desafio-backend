9c488087b55ffc79dfcaa8803d363db373331946
Leonardo <leocordova96@gmail.com>
2023-10-26 17:39:10 -0300

    rafactor: change endpoint ranking/create to ranking/rate

--- ranking/urls.py
+++ ranking/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from ranking.views import UserRateMovieView, UserRankingListView, RatingDeleteView, GeneralListMovieRatingView
 
 urlpatterns = [
-    path('create/', UserRateMovieView.as_view(), name='user-rate-movie'),
+    path('rate/', UserRateMovieView.as_view(), name='user-rate-movie'),
     path('list/', UserRankingListView.as_view(), name='user-rankings' ),
     path('delete/<int:pk>', RatingDeleteView.as_view(), name='delete-rating'),
     path('general-list/', GeneralListMovieRatingView.as_view(), name='general-movie-rating')
