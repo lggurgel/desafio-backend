29e023bdcab8601859fb0df2c426d84e78c6d5fa
Leonardo <leocordova96@gmail.com>
2023-10-26 17:36:39 -0300

    update: users/admin

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from users.models import CustomUser
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'password']
+    list_display = ['id', 'username', 'email', 'password']
 
 admin.site.register(CustomUser, CustomUserAdmin)
