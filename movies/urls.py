1140c045d3c2543a08d927f6e8a16b1348808cc2
Leonardo <leocordova96@gmail.com>
2023-11-03 16:53:35 -0300

    fix:correcting the lack of /

--- movies/urls.py
+++ movies/urls.py
@@ -4,7 +4,7 @@ from movies.views import AddMovieView, MovieDetailView, UpdateMovieView, Created
 urlpatterns = [
     path('add/', AddMovieView.as_view(), name='add-movies'),
     path('movie/<int:pk>/', MovieDetailView.as_view(), name='movie'),
-    path('update/<int:pk>', UpdateMovieView.as_view(), name='update-movies'),
+    path('update/<int:pk>/', UpdateMovieView.as_view(), name='update-movies'),
     path('created/', CreatedMovieListView.as_view(), name='created-movies'),
     path('list/', ListAllMovies.as_view(), name='list-all-movies'),
     path('recommended/', RecommendedMoviesView.as_view(), name='recommended-movies')
