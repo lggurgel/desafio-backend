bac1f3f909769bccef9a91014c8ca565bf598446
Leonardo <leocordova96@gmail.com>
2023-11-09 16:22:56 -0300

    tests: added in test add movie view error messages

--- movies/tests/test_movies_views.py
+++ movies/tests/test_movies_views.py
@@ -10,13 +10,13 @@ from users.models import CustomUser
     'data, expected_status',
     [
         (['Movie test', 2023, 'Director test', 'Action'], 201),
-        (['', 2023, 'Director test', 'Action'], 400),
-        (['Movie test2', '', 'Director test', 'Action'], 400),
-        (['Movie test3', 2023, '', 'Action'], 400),
-        (['Movie test4', 2023, 'Director test', ''], 400),
+        (['', 2023, 'Director test', 'Action'], [400, 'This field may not be blank.']),
+        (['Movie test2', '', 'Director test', 'Action'], [400, 'A valid integer is required.']),
+        (['Movie test3', 2023, '', 'Action'], [400, 'This field may not be blank.']),
+        (['Movie test4', 2023, 'Director test', ''], [400, 'is not a valid choice.']),
     ]
 )
-def test_add_movie_view_valid_and_invalid_data(data, expected_status):
+def test_add_movie_view_valid_and_invalid_data_messages_error(data, expected_status):
     title, year, director, genre = data
 
     user = CustomUser.objects.create_user(
@@ -42,41 +42,9 @@ def test_add_movie_view_valid_and_invalid_data(data, expected_status):
     response = client.post(url, data, format='json')
     response_json = response.json()
 
-    assert response.status_code == expected_status
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    'data, expected_message',
-    [
-        (['', 2023, 'Director test', 'Action'], 'This field may not be blank.'),
-        (['Movie test2', '', 'Director test', 'Action'], 'A valid integer is required.'),
-        (['Movie test3', 2023, '', 'Action'], 'This field may not be blank.'),
-        (['Movie test4', 2023, 'Director test', ''], 'is not a valid choice.'),
-    ]
-) 
-def test_add_movie_view_invalid_data_messages_error(data, expected_message):
-    title, year, director, genre = data
-
-    user = CustomUser.objects.create_user(
-        username='testuser',
-        password='testpassword'
-    )
-   
-    data = {
-        'title': title,
-        'year': year,
-        'director': director,
-        'genre': genre,
-        'user': user.id
-    }
-
-    client = APIClient()
-    url = reverse('add-movies')
-    client.login(username='testuser', password='testpassword')
-    
-    # pytest.set_trace()
-
-    response = client.post(url, data, format='json')
-
-    assert expected_message in str(response.content)
-
+    if response.status_code == 400:
+        assert response.status_code == expected_status[0]
+        assert expected_status[1] in str(response.content)
+    else:
+        assert response.status_code == expected_status
+        
