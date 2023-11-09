ffd9c88a4603c6400b120fb75f9ab20dfa31c23a
Leonardo <leocordova96@gmail.com>
2023-11-09 09:47:55 -0300

    test: update tests

--- movies/tests/test_movies_views.py
+++ movies/tests/test_movies_views.py
@@ -1,88 +1,42 @@
 import pytest
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from users.models import CustomUser
 
-class MoviesViewsTest(APITestCase):
-    def setUp(self):
-        self.user = CustomUser.objects.create_user(
-            username='testuser',
-            password='testpassword'
-        )
-        self.add_url = reverse('add-movies')
-
-    @pytest.mark.django_db
-    @pytest.mark.parametrize('data, expected_status', [
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'data, expected_status',
+    [
         (['Movie test', 2023, 'Director test', 'Action'], 201),
         (['', 2023, 'Director test', 'Action'], 400),
         (['Movie test', '', 'Director test', 'Action'], 400),
         (['Movie test', 2023, '', 'Action'], 400),
         (['Movie test', 2023, 'Director test', ''], 400),
-    ])
-    def test_add_movie_view_valid_data(self, data, expected_status):
-        self.client.login(username='testuser', password='testpassword')
+    ]
+)
+def test_add_movie_view_valid_data(data, expected_status):
+    title, year, director, genre = data
 
-        title, year, director, genre = data
+    user = CustomUser.objects.create_user(
+        username='testuser',
+        password='testpassword'
+    )
 
-        data = {
-            'title': title,
-            'year': year,
-            'director': director,
-            'genre': genre,
-            'user': self.user.id
-        }
-        response = self.client.post(self.add_url, data, format='json')
-        assert response.status_code == expected_status
-       
-        # assert response.data == expected_data
-        
-        # self.assertDictEqual(expected_json, response.data)
+    data = {
+        'title': title,
+        'year': year,
+        'director': director,
+        'genre': genre,
+        'user': user.id
+    }
 
-    # def test_add_movie_view_invalid_title(self):
-    #     self.client.login(username='testuser', password='testpassword')
+    client = APIClient()
+    url = reverse('add-movies')
+    client.login(username='testuser', password='testpassword')
 
-    #     data = {
-    #         'title': '',
-    #         'year': 2023,
-    #         'director': 'Test director',
-    #         'genre': 'Comedy',
-    #         'user': self.user.id
-    #     }
+    # pytest.set_trace()
 
-    #     response = self.client.post(self.add_url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIn('title', response.data)
-    #     self.assertEqual(response.data['title'][0], 'This field may not be blank.')
+    response = client.post(url, data, format='json')
 
-    # def test_add_movie_view_invalid_year(self):
-    #     self.client.login(username='testuser', password='testpassword')
-
-    #     data = {
-    #         'title': 'Filme teste 1',
-    #         'year': '',
-    #         'director': 'Test director',
-    #         'genre': 'Comedy',
-    #         'user': self.user.id
-    #     }
-
-    #     response = self.client.post(self.add_url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIn('year', response.data)
-    #     self.assertEqual(response.data['year'][0], 'A valid integer is required.')
-
-    # def test_add_movie_view_invalid_director(self):
-    #     self.client.login(username='testuser', password='testpassword')
-
-    #     data = {
-    #         'title': 'Filme teste 1',
-    #         'year': 2023,
-    #         'director': '',
-    #         'genre': 'Comedy',
-    #         'user': self.user.id
-    #     }
-
-    #     response = self.client.post(self.add_url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIn('director', response.data)
-    #     self.assertEqual(response.data['director'][0], 'This field may not be blank.')
+    assert response.status_code == expected_status
